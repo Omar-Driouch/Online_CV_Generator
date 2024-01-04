2ed699a63f4136e37900ae1c00d6ed30f1b17537
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-04 14:23:04 +0100

    update skills

--- src/features/Skills.jsx
+++ src/features/Skills.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { FiPlus } from "react-icons/fi";
+import { FaChevronDown } from "react-icons/fa";
 const Skills = () => {
+  const [isHide, setIsHide] = useState(false);
+  const [skill , setSkill] = useState("");
+  const [skills , setSkills]  = useState([]);
+  const handleChange = (e)=>{
+    setSkill(e.target.value);
+  }
+  const handleClick = ()=>{
+    if(skill){
+      setSkills([...skills,skill]);
+      setSkill("");
+    }
+  }
   return (
     <section className="skills-section">
-      <h1>skills</h1>
-      <div className="skills-form">
-        <input type="text" placeholder="ajouter votre competences" />
-        <button>
-          <span>
-            <FiPlus />
-          </span>
-          <span>ajouter</span>
-        </button>
+      <div className="section-heading"  onClick={() => setIsHide(!isHide)}>
+        <h1>skills</h1>
+        <button style={{border:"none",background:"transparent"}}><FaChevronDown /></button>
       </div>
+      {!isHide && (
+        <div className="skills-form">
+          <input type="text" value={skill} onChange={handleChange} placeholder="ajouter votre competences" />
+          <button className="add-skill-btn" onClick={handleClick}>
+            <span>
+              <FiPlus />
+            </span>
+            <span>ajouter</span>
+          </button>
+        </div>
+      )}
     </section>
   );
 };
