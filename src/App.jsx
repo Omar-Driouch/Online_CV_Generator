12e0bffe15cc872cfb8094dde8df2a3689eff8ac
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-05 10:52:07 +0100

    add personal informations

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Personalinfo from "./features/PersonalInfo";
 import Education from "./features/Education";
 import Skills from "./features/Skills";
+import References from "./features/References";
 
 function App() {
   const [data , setData] = useState([]);
@@ -14,8 +16,10 @@ function App() {
     <div className="container-app">
       <Header />
       <div className="sections-container">
+        <Personalinfo/>
         <Education />
         <Skills sendData={(skills)=>handleRecive(skills)} />
+        <References/>
       {/* <ul className="data">
         {data.map((d,index)=>(
           <li key={index}>{d}</li>
--- src/features/References.jsx
+++ src/features/References.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { FaChevronDown } from "react-icons/fa";
