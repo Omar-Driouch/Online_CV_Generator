4823f066968d274ed12aabd4f8f2c2d2a35513a5
Omar-Driouch <omardriouch29@gmail.com>
2024-01-09 10:48:04 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,32 @@ import logo from "../../public/logo.svg"
 const Header = () => {
   return (
     <header>
-        <div className="logo">
-            <img src={logo} alt="app-logo" width={130} />
-        </div>
-        <nav className="menu">
-          <ul>
-            <li><a href="#">home</a></li>
-            <li><a href="#">about us</a></li>
-            <li><a href="#">contact us</a></li>
-            <li><a href="#">team</a></li>
-            <li><a href="#">others</a></li>
-          </ul>
-        </nav>
+      <div className="logo">
+        <img src={logo} alt="app-logo" width={130} />
+      </div>
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about us</a>
+          </li>
+          <li>
+            <a href="#">contact us</a>
+          </li>
+          <li>
+            <a href="https://main--monumental-choux-4ab945.netlify.app/">
+              team
+            </a>
+          </li>
+          <li>
+            <a href="#">others</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header
